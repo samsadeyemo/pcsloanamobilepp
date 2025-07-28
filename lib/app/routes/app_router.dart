cff29d37e37c90003b6c36aaa185bb38be111e03
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2025-07-28 02:03:31 +0100

    feature(forget-password-screen) created forget password screen successfully

--- lib/app/routes/app_router.dart
+++ lib/app/routes/app_router.dart
@@ -6,13 +6,17 @@ import 'package:pcsloan/features/auth/presentation/screens/login_screen.dart';
 import 'package:pcsloan/features/auth/presentation/screens/sign_up_screen.dart';
 import 'package:pcsloan/features/auth/presentation/screens/transaction_pin_screen.dart';
 import 'package:pcsloan/features/auth/presentation/screens/verify_phone_screen.dart';
+import 'package:pcsloan/features/password_management/presentation/forget_password_screen.dart';
 import '../../features/splash/presentation/splash_screen.dart';
 import '../../features/onboarding/presentation/onboarding_screen.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashScreen()
+    ),
     GoRoute(
       path: '/onboarding',
       builder: (context, state) => OnboardingScreen(),
@@ -21,8 +25,14 @@ final GoRouter router = GoRouter(
       path: '/getStartedScreen',
       builder: (context, state) => GetStartedScreen(),
     ),
-    GoRoute(path: "/signIn", builder: (context, state) => LoginScreen()),
-    GoRoute(path: "/signUp", builder: (context, state) => SignUpScreen()),
+    GoRoute(
+      path: "/signIn",
+      builder: (context, state) => LoginScreen()
+    ),
+    GoRoute(
+      path: "/signUp",
+      builder: (context, state) => SignUpScreen()
+    ),
       GoRoute(
       path: '/create-password',
       builder: (context, state) => const CreatePasswordScreen(),
@@ -39,5 +49,9 @@ final GoRouter router = GoRouter(
       path: '/account-created-screen',
       builder: (context, state) => const AccountCreatedScreen(),
     ),
+    GoRoute(
+      path: '/forgot-password-screen',
+      builder: (context, state) => const ForgetPasswordScreen(),
+      )
   ],
 );

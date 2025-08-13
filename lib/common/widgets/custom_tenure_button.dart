c8c01b78d4c58e11bea974f98854269006a57c4f
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2025-08-13 13:17:32 +0100

    Add custom tenure button

---
+++ lib/common/widgets/custom_tenure_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MonthButton extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const MonthButton({
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        decoration: BoxDecoration(
+          color: isSelected ? Color(0xff7C70DF) : Colors.grey[200],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? Colors.white : Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
